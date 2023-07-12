9b86081b7bcf1cc16e1ef2750b0ee66fd68bd5a4
LEE DONG GYU <tenenger7125@gmail.com>
2023-07-12 19:11:09 +0900

    feat: add issues type

---
+++ src/types/issues.ts
@@ -0,0 +1,114 @@
+type User = {
+  login: string;
+  id: number;
+  node_id: string;
+  avatar_url: string;
+  gravatar_id: string;
+  url: string;
+  html_url: string;
+  followers_url: string;
+  following_url: string;
+  gists_url: string;
+  starred_url: string;
+  subscriptions_url: string;
+  organizations_url: string;
+  repos_url: string;
+  events_url: string;
+  received_events_url: string;
+  type: string;
+  site_admin: false;
+};
+
+type Label = {
+  id: number;
+  node_id: string;
+  url: string;
+  name: string;
+  color: string;
+  default: false;
+  description: string;
+};
+
+type Creator = {
+  login: string;
+  id: number;
+  node_id: string;
+  avatar_url: string;
+  gravatar_id: string;
+  url: string;
+  html_url: string;
+  followers_url: string;
+  following_url: string;
+  gists_url: string;
+  starred_url: string;
+  subscriptions_url: string;
+  organizations_url: string;
+  repos_url: string;
+  events_url: string;
+  received_events_url: string;
+  type: string;
+  site_admin: false;
+};
+
+type Milestone = {
+  url: string;
+  html_url: string;
+  labels_url: string;
+  id: number;
+  node_id: string;
+  number: number;
+  title: string;
+  description: string;
+  creator: Creator;
+  open_issues: number;
+  closed_issues: number;
+  state: string;
+  created_at: string;
+  updated_at: string;
+  due_on: null;
+  closed_at: null;
+};
+
+type Reaction = {
+  url: string;
+  total_count: number;
+  '+1': number;
+  '-1': number;
+  laugh: number;
+  hooray: number;
+  confused: number;
+  heart: number;
+  rocket: number;
+  eyes: number;
+};
+
+export type Issue = {
+  url: string;
+  repository_url: string;
+  labels_url: string;
+  comments_url: string;
+  events_url: string;
+  html_url: string;
+  id: number;
+  node_id: string;
+  number: number;
+  title: string;
+  user: User;
+  labels: Label[];
+  state: string;
+  locked: false;
+  assignee: null;
+  assignees: [];
+  milestone: Milestone;
+  comments: number;
+  created_at: string;
+  updated_at: string;
+  closed_at: null;
+  author_association: string;
+  active_lock_reason: null;
+  body: string;
+  reactions: Reaction;
+  timeline_url: string;
+  performed_via_github_app: null;
+  state_reason: null;
+};
