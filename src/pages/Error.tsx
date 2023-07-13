3623a7ccd3d923681ae6c2aa8d293da94033b14c
LEE DONG GYU <tenenger7125@gmail.com>
2023-07-13 20:15:40 +0900

    feat: add back button at error page

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,10 +1,14 @@
 import { styled } from 'styled-components';
 import { Title } from '@/components';
+import { useNavigate } from 'react-router-dom';
 
 const Error = () => {
+  const navigate = useNavigate();
+
   return (
     <SLayout>
       <Title order={1}>원하시는 페이지를 찾을 수 없습니다.</Title>
+      <SBackButton onClick={() => navigate(-1)}>뒤로가기</SBackButton>
       <SContainer>
         <img alt="404 not found" src="/images/notFound.jpg"></img>
       </SContainer>
@@ -25,6 +29,22 @@ const SContainer = styled.div`
   img {
     max-width: 100%;
   }
+`;
+
+const SBackButton = styled.button`
+  width: 200px;
+  padding: 10px 20px;
+  margin: 10px;
+
+  font-size: 1rem;
+  font-weight: bold;
+  color: ${props => props.theme.colors.white[0]};
+
+  background-color: ${props => props.theme.colors.blue[6]};
+  border: none;
+  border-radius: 5px;
+
+  cursor: pointer;
 `;
 
 export default Error;
